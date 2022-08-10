10bf4e3e3f895777eaee8f08880b9d342de86794
alisalim17 <alisalimli1899@gmail.com>
2022-08-10 14:58:08 +0400

    adding images

--- constants/constants.ts
+++ constants/constants.ts
@@ -24,6 +24,34 @@ export enum DRAWER_SCREENS {
   HELP_CENTER = "Help Center",
 }
 
+///----
+const onboarding_screens = [
+  {
+    id: 1,
+    backgroundImage: require("../assets/images/background_01.png"),
+    bannerImage: require("../assets/images/favourite_food.png"),
+    title: "Choose a Favourite Food",
+    description:
+      "When you oder Eat Steet, we’ll hook you up with exclusive coupon, specials and rewards",
+  },
+  {
+    id: 2,
+    backgroundImage: require("../assets/images/background_02.png"),
+    bannerImage: require("../assets/images/hot_delivery.png"),
+    title: "Hot Delivery to Home",
+    description:
+      "We make food ordering fasr, simple and free-no matter if you order online or cash",
+  },
+  {
+    id: 3,
+    backgroundImage: require("../assets/images/background_01.png"),
+    bannerImage: require("../assets/images/great_food.png"),
+    title: "Receive the Great Food",
+    description:
+      "You’ll receive the great food within a hour. And get free delivery credits for every order.",
+  },
+];
+
 const bottom_tabs = [
   {
     id: 0,
@@ -130,6 +158,7 @@ export default {
   bottom_tabs,
   delivery_time,
   ratings,
+  onboarding_screens,
 
   tags,
 };
