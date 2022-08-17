967acc7a4384d68352ee1122c4c25cb228007590
alisalim17 <alisalimli1899@gmail.com>
2022-08-17 22:19:48 +0400

    Setting up Add Card

--- src/screens/MyCard/MyCard.tsx
+++ src/screens/MyCard/MyCard.tsx
@@ -1,10 +1,14 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
+import Button from "../../components/Form/Button";
 import { Header, HeaderBack } from "../../components/Header";
 import { tw } from "../../utils";
+import { RootStackNavigationProps } from "../Screens";
 import MyCardList from "./MyCardList";
 
 const MyCard = () => {
+  const navigation = useNavigation<RootStackNavigationProps>();
   return (
     <View style={tw`flex-1 bg-white`}>
       {/* Header */}
@@ -17,12 +21,19 @@ const MyCard = () => {
           <Text></Text>
         </Header.Right>
       </Header>
-      <View style={tw`py-2 px-4`}>
-        {/* Card Header */}
-        <Text style={tw`h3 ml-1 my-4`}>Add new card</Text>
-        {/* Cards List */}
-        <MyCardList />
+      <View style={tw`flex-1 py-2 px-4`}>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          style={tw`flex-1 mb-8`}
+        >
+          {/* Card Header */}
+          <Text style={tw`h3 ml-1 my-4`}>Add new card</Text>
+          <MyCardList />
+        </ScrollView>
         {/* Footer */}
+        <Button onPress={() => navigation.push("AddCard")} variant="primary">
+          Add
+        </Button>
       </View>
     </View>
   );
