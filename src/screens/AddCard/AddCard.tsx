6060e0f5c238f10b7d703af70990732ccb3bf814
alisalim17 <alisalimli1899@gmail.com>
2022-08-17 22:19:48 +0400

    Setting up AddCard

---
+++ src/screens/AddCard/AddCard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { tw } from "../../utils";
+
+const AddCard = () => {
+  return (
+    <View style={tw`flex-1 bg-white`}>
+      <Text>noti</Text>
+    </View>
+  );
+};
+
+export default AddCard;
