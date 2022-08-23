4908aa49367cb928e9063201e30660858896785b
alisalim17 <alisalimli1899@gmail.com>
2022-08-23 21:35:04 +0400

    header

--- src/screens/Delivery/Delivery.tsx
+++ src/screens/Delivery/Delivery.tsx
@@ -19,7 +19,6 @@ const TrackOrderItem: React.FC<ITrackOrderItemProps> = ({
   index,
 }) => {
   const isLastElement = constants.track_order_status?.length - 1 === index;
-  const isStepCompleted = index < currentStep;
 
   return (
     <View>
@@ -28,13 +27,16 @@ const TrackOrderItem: React.FC<ITrackOrderItemProps> = ({
           {/* Check mark icon */}
           <Image
             source={icons.check_circle}
-            style={[tw`w-10 h-10`, !isStepCompleted && tw`tint-lightGray1`]}
+            style={[
+              tw`w-10 h-10`,
+              !(index < currentStep) && tw`tint-lightGray1`,
+            ]}
           />
 
           {/* If is not last element show completed or uncompleted icon */}
           {!isLastElement ? (
             <Fragment>
-              {isStepCompleted ? (
+              {index < currentStep - 1 ? (
                 <View style={tw`w-1 h-12 bg-primary`}></View>
               ) : (
                 <Image style={tw`w-1 h-12`} source={icons.dotted_line} />
@@ -53,10 +55,10 @@ const TrackOrderItem: React.FC<ITrackOrderItemProps> = ({
 };
 
 const DeliveryStatus = () => {
-  const [currentStep, setCurrentStep] = useState(3);
+  const [currentStep, setCurrentStep] = useState(2);
 
   return (
-    <View style={tw`flex-1`}>
+    <View style={tw`flex-1 bg-white`}>
       <Header>
         <Header.Left>
           <HeaderBack />
@@ -64,15 +66,31 @@ const DeliveryStatus = () => {
         <Header.Title>Delivery Status</Header.Title>
       </Header>
       {/* @todo create a container */}
-      <View style={tw`py-4 px-8`}>
-        {constants.track_order_status.map((item, index) => (
-          <TrackOrderItem
-            currentStep={currentStep}
-            index={index}
-            key={`delivery-status-${item.id}`}
-            {...item}
-          />
-        ))}
+      <View style={tw`py-4 px-6`}>
+        {/* Header */}
+        <View style={tw`justify-center items-center mb-6`}>
+          <Text style={tw`body4 text-gray`}>Estimated Delivery</Text>
+          <Text style={tw`h1 font-black`}>12 Sept 2024 / 17:00 PM</Text>
+        </View>
+        <View style={tw`bg-white2 rounded-lg border border-lightGray2`}>
+          <View
+            style={tw`flex-row justify-between border-b-2 border-b-lightGray2 px-4 pt-5 pb-3 mb-4`}
+          >
+            <Text style={tw`h3`}>Track Order</Text>
+            <Text style={tw`text-gray`}>NY012345</Text>
+          </View>
+          <View style={tw`py-2 px-3 mb-2`}>
+            {/* Order Status */}
+            {constants.track_order_status.map((item, index) => (
+              <TrackOrderItem
+                currentStep={currentStep}
+                index={index}
+                key={`delivery-status-${item.id}`}
+                {...item}
+              />
+            ))}
+          </View>
+        </View>
       </View>
     </View>
   );
