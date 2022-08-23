26043639d6f276fca0e2517838a40c5f20f95172
alisalim17 <alisalimli1899@gmail.com>
2022-08-23 15:20:21 +0400

    adding track_order_status data

--- constants/constants.ts
+++ constants/constants.ts
@@ -161,6 +161,33 @@ const tags = [
     label: "Taccos",
   },
 ];
+const track_order_status = [
+  {
+    id: 1,
+    title: "Order Confirmed",
+    sub_title: "Your order has been received",
+  },
+  {
+    id: 2,
+    title: "Order Prepared",
+    sub_title: "Your order has been prepared",
+  },
+  {
+    id: 3,
+    title: "Delivery in Progress",
+    sub_title: "Hang on! Your food is on the way",
+  },
+  {
+    id: 4,
+    title: "Delivered",
+    sub_title: "Enjoy your meal!",
+  },
+  {
+    id: 5,
+    title: "Rate Us",
+    sub_title: "Help us improve our service",
+  },
+];
 
 export default {
   screens,
@@ -168,6 +195,6 @@ export default {
   delivery_time,
   ratings,
   onboarding_screens,
-
+  track_order_status,
   tags,
 };
--- src/screens/Delivery/Delivery.tsx
+++ src/screens/Delivery/Delivery.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { constants } from "../../../constants";
 import { Header, HeaderBack } from "../../components/Header";
 import { tw } from "../../utils";
 
@@ -10,7 +11,10 @@ const DeliveryStatus = () => {
         <Header.Left>
           <HeaderBack />
         </Header.Left>
+        <Header.Title>Delivery Status</Header.Title>
       </Header>
+      {/* @todo create a container */}
+      <View style={tw`py-4 px-8`}></View>
     </View>
   );
 };
